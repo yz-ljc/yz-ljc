c45a3c468d567d32e6050b5f95b873b08ebf9f66
YZ_Ljc_ <yzljc@stu.tjufe.edu.cn>
2026-01-22 22:42:56 +0800

    chore(test): test
    test
    123

--- test/test.java
+++ test/test.java
@@ -2,29 +2,8 @@ package top.yzljc.qqbot.config;
 
 import java.util.List;
 
-public interface Settings {
-
-    int getListenPort();
-
-    String getMysqlHost();
-
-    int getMysqlPort();
-
-    String getMysqlDatabase();
-
-    String getMysqlUsername();
-
-    String getMysqlPassword();
-
-    long getManosabaGroupId();
-
-    boolean isDebugMode();
-
-    List<Long> getMessageSpyGroups();
-
     String getHttpUrl();
 
-    String getMysqlHost();
 
     int getMysqlPort();
 
@@ -35,62 +14,6 @@ public interface Settings {
     String getMysqlPassword();
 
     long getManosabaGroupId();
-
-    boolean isDebugMode();
-
-    List<Long> getMessageSpyGroups();
-
-    String getHttpUrl();
-
-    String getMysqlHost();
-
-    int getMysqlPort();
-
-    String getMysqlDatabase();
-
-    String getMysqlUsername();
-
-    String getMysqlPassword();
-
-    long getManosabaGroupId();
-
-    boolean isDebugMode();
-
-    List<Long> getMessageSpyGroups();
-
-    String getHttpUrl();
-
-    String getMysqlHost();
-
-    int getMysqlPort();
-
-    String getMysqlDatabase();
-
-    String getMysqlUsername();
-
-    String getMysqlPassword();
-
-    long getManosabaGroupId();
-
-    boolean isDebugMode();
-
-    List<Long> getMessageSpyGroups();
-
-    String getHttpUrl();
-
-    String getMysqlHost();
-
-    int getMysqlPort();
-
-    String getMysqlDatabase();
-
-    String getMysqlUsername();
-
-    String getMysqlPassword();
-
-    long getManosabaGroupId();
-
-    boolean isDebugMode();
 
     List<Long> getMessageSpyGroups();
 
